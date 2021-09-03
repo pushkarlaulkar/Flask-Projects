939eaa10fa4565aa546e6b5e2c319692fcf1a206
Pushkar Sunil Laulkar <plaulkar@etisalat.ae>
2021-09-03 22:26:42 +0400

    Updated on Fri, 3 Sep 2021

--- Full-Featured-Web-App/flaskblog/__init__.py
+++ Full-Featured-Web-App/flaskblog/__init__.py
@@ -1,14 +1,25 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+from flaskblog.config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' #Required for Forms like CSRF in Django
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-from flaskblog import routes
+
+
+mail = Mail(app)
+
+from flaskblog.users.routes import users
+app.register_blueprint(users)
+from flaskblog.posts.routes import posts
+app.register_blueprint(posts)
+from flaskblog.main.routes import main
+app.register_blueprint(main)
