5ee1cdeb7d799568a9604517de96333b206553b5
Pushkar Sunil Laulkar <plaulkar@etisalat.ae>
2021-09-12 14:32:30 +0400

    Updated new things

--- edureka-flask/app.py
+++ edureka-flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request, render_template, make_response
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,7 +7,7 @@ def hello_world():
 
 @app.route('/hello/<name>')
 def hello_world1(name):
-	return 'Hello %s' % name
+	return render_template('hello.html', name = name)
 
 @app.route('/blog/<int:postid>')
 def blog(postid):
@@ -32,5 +32,33 @@ def hello_user(name):
 	else:
 		return redirect(url_for('guest', guest = name))
 
+@app.route('/success/<name>')
+def success(name):
+	return 'Welcome %s' % name
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+	if(request.method == 'POST'):
+		user = request.form['nm']
+		return redirect(url_for('success', name = user))
+	else:
+		user = request.args.get('nm')
+		return redirect(url_for('success', name = user))
+
+@app.route('/setcookie', methods = ['POST', 'GET'])
+def setcookie():
+	if(request.method == 'POST'):
+		user = request.form['nm']
+	
+	resp = make_response(render_template('readcookie.html'))
+	resp.set_cookie('userid', user)
+	
+	return resp
+
+@app.route('/getcookie')
+def getcookie():
+	name = request.cookies.get('userid')
+	return '<h1>Welcome '+name+'</h1>'
+		
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", debug=True)
