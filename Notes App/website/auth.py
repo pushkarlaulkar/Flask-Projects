b582c33a1ec234c43974cf9b1e2e150926c95f46
Pushkar Sunil Laulkar <pushkar.laulkar@yahoo.com>
2021-10-09 13:20:09 +0400

    Updated Login & Sign Up routes

--- Notes App/website/auth.py
+++ Notes App/website/auth.py
@@ -1,15 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-	return "<p>Login</p>"
+	data = request.form
+	if(request.method == 'POST'):	print("Email :- " + data['email'] + "\n" + "Password :- " + data['password'])
+	return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
 	return "<p>Logout</p>"
 	
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
-	return "<p>Sign Up</p>"
+	if(request.method == 'POST'):
+		email = request.form.get('email')
+		firstname = request.form.get('firstname')
+		password1 = request.form.get('password1')
+		password2 = request.form.get('password2')
+		
+		if(len(email) < 4):
+			flash("Email must be greater than 4 characters.", category='error')
+		elif(len(firstname) < 2):
+			flash("First Name must be greater than 1 character.", category='error')
+		elif(password1 != password2):
+			flash("Passwords do not match.", category='error')
+		elif(len(password1) < 7):
+			flash("Password must be at least 7 characters.", category='error')
+		else:
+			flash("Account created!", category='success')
+	return render_template("signup.html")
