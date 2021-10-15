6dbbe252b71d965cbd54451312736774971c98f2
Pushkar Sunil Laulkar <pushkar.laulkar@yahoo.com>
2021-10-15 11:57:28 +0400

    Completed the Notes App

--- Notes App/website/auth.py
+++ Notes App/website/auth.py
@@ -1,33 +1,61 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, logout_user, login_required, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-	data = request.form
-	if(request.method == 'POST'):	print("Email :- " + data['email'] + "\n" + "Password :- " + data['password'])
-	return render_template("login.html")
+	if(request.method == 'POST'):
+		email = request.form.get('email')
+		password = request.form.get('password')
+		
+		user = User.query.filter_by(email=email).first()
+		if user:
+			if check_password_hash(user.password, password):
+				flash('Logged in successfully!!', category='success')
+				login_user(user, remember=True)
+				return redirect(url_for('views.home'))
+			else:
+				flash('Incorrect Password!!', category='error')
+		else:
+			flash('Email doesn\'t exist!!!', category='error')
+	return render_template("login.html", user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-	return "<p>Logout</p>"
+	logout_user()
+	return redirect(url_for('auth.login'))
 	
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
 	if(request.method == 'POST'):
 		email = request.form.get('email')
-		firstname = request.form.get('firstname')
+		firstName = request.form.get('firstname')
 		password1 = request.form.get('password1')
 		password2 = request.form.get('password2')
 		
-		if(len(email) < 4):
+		user = User.query.filter_by(email=email).first()
+		
+		if user:
+			flash('Email already exists!!', category='error')
+		elif(len(email) < 4):
 			flash("Email must be greater than 4 characters.", category='error')
-		elif(len(firstname) < 2):
+		elif(len(firstName) < 2):
 			flash("First Name must be greater than 1 character.", category='error')
 		elif(password1 != password2):
 			flash("Passwords do not match.", category='error')
 		elif(len(password1) < 7):
 			flash("Password must be at least 7 characters.", category='error')
 		else:
+			new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, method='sha256'))
+			db.session.add(new_user)
+			db.session.commit()
+			login_user(user)
 			flash("Account created!", category='success')
-	return render_template("signup.html")
+			return redirect(url_for('views.home'))
+			
+	return render_template("signup.html", user=current_user)
--- Notes App/website/views.py
+++ Notes App/website/views.py
@@ -1,7 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-	return render_template("home.html")
+	if(request.method == 'POST'):
+		note = request.form.get('note')
+		new_note = Note(data=note, user_id = current_user.id)
+		db.session.add(new_note)
+		db.session.commit()
+		flash('Note Added!!', category='success')
+	return render_template("home.html", user=current_user)
+	
+@views.route('/delete/<int:noteid>')
+def delete(noteid):
+	note_to_delete = Note.query.get(noteid)
+	db.session.delete(note_to_delete)
+	db.session.commit()
+	flash('Note Deleted!!', category='error')
+	return redirect(url_for('views.home'))
